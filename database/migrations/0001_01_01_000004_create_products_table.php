40bf17f8b99a8847189aa987baf36bd7a039a82a
NaftaliSumendap <sumendapn7@gmail,com>
2025-05-14 00:24:30 +0800

    update

--- database/migrations/0001_01_01_000004_create_products_table.php
+++ database/migrations/0001_01_01_000004_create_products_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('brand')->nullable();
             $table->boolean('is_featured')->default(false);
             $table->timestamps();
+            $table->string('image_path')->nullable();
         });
     }
 
