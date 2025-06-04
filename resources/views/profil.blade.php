513b4f855155e571f5bbd16a02f91b8c8c68f7c1
NaftaliSumendap <sumendapn7@gmail,com>
2025-06-04 10:09:17 +0800

    update

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -19,7 +19,7 @@
         <h2 class="text-lg font-medium text-gray-700 mb-4">Profil Pribadi</h2>
         <div class="flex flex-col items-center">
           <div class="w-40 h-40 bg-red-300 rounded-lg overflow-hidden mb-2">
-            <img src="../img/{{$user['image']}}</" alt="Profile" class="w-full h-full object-cover">
+            <img src="img/{{$user['image']}}" alt="Profile" class="w-full h-full object-cover">
           </div>
           <button class="bg-white text-gray-700 border border-gray-300 rounded-md px-4 py-2 text-sm mt-2 w-full">Pilih Foto</button>
           <!-- Logout Button -->
@@ -58,7 +58,7 @@
             <div class="flex items-center justify-between">
                 <span class="text-gray-600">Alamat</span>
                 <div class="flex items-center">
-                  <span class="text-gray-700 mr-2" id="alamat-value">{{$user['address']}}</</span>
+                  <span class="text-gray-700 mr-2" id="alamat-value">{{$user['address']}}</span>
                   <button onclick="openModal('Alamat', 'alamat-value', 'textarea')" class="text-amber-400 hover:text-amber-500 text-lg font-bold">&gt;</button>
                 </div>
               </div>
@@ -78,7 +78,7 @@
             <div class="flex items-center justify-between">
               <span class="text-gray-600">Nomor HP</span>
               <div class="flex items-center">
-                <span class="text-gray-700 mr-2" id="nomor-hp-value">{{$user['phone']}}</</span>
+                <span class="text-gray-700 mr-2" id="nomor-hp-value">{{$user['phone']}}</span>
                 <button onclick="openModal('Nomor HP', 'nomor-hp-value', 'tel')" class="text-amber-400 hover:text-amber-500 text-lg font-bold">&gt;</button>
               </div>
             </div>
