93834f13a9aecb6fc698c3859fefe7606dd0da0b
mogus <sumendapn7@gmail.com>
2025-06-12 19:38:13 +0800

    update

--- database/migrations/0001_01_01_000007_create_order_Items_table.php
+++ database/migrations/0001_01_01_000007_create_order_Items_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('order_id')->constrained();
+            $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
             $table->foreignId('product_id')->constrained();
             $table->integer('quantity');
             $table->decimal('price', 10, 2);
