973b7cef2e92374456f6e31142ece3f6c2395b44
mogus <sumendapn7@gmail.com>
2025-06-12 12:51:38 +0800

    update

--- database/migrations/0001_01_01_000006_create_orders_table.php
+++ database/migrations/0001_01_01_000006_create_orders_table.php
@@ -11,7 +11,6 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained();
-            $table->foreignId('product_id')->constrained();
             $table->string('order_number')->unique();
             $table->decimal('total_amount', 10, 2);
             $table->enum('status', ['Menunggu', 'Dikonfirmasi', 'Packaging', 'Pengantaran', 'Diterima', 'Dibatalkan'])->default('Menunggu');
